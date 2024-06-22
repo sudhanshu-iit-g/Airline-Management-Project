a94be6a2e901eb749c2374361c2823ebe552d22f
sudhanshu-iit-g <134191776+sudhanshu-iit-g@users.noreply.github.com>
2024-06-22 22:45:41 +0530

    Add files via upload

---
+++ backend/middlewares/authMiddleware.js
@@ -0,0 +1,35 @@
+// this code is not in use
+
+const jwt = require('jsonwebtoken');
+
+const userAuth = (req, res, next) => {
+  const token = req.header('x-auth-token');
+  if (!token) {
+    return res.status(401).json({ message: 'No token, authorization denied' });
+  }
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = decoded.user;
+    next();
+  } catch (err) {
+    res.status(401).json({ message: 'Token is not valid' });
+  }
+};
+
+const adminAuth = (req, res, next) => {
+  const token = req.header('x-auth-token');
+  if (!token) {
+    return res.status(401).json({ message: 'No token, authorization denied' });
+  }
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    req.admin = decoded.admin;
+    next();
+  } catch (err) {
+    res.status(401).json({ message: 'Token is not valid' });
+  }
+};
+
+module.exports = { userAuth, adminAuth };
